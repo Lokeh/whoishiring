7aaf0f89eac8bfaf70fff87713fe6769582df6fa
Will <will@willacton.com>
2016-12-20 16:29:17 -0800

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
 import * as React from "react";
+import * as Cactus from 'cactus';
+import { compose }from 'ramda';
 
-export default class App extends React.Component<any, any> {
-    render() {
+function view(model$: any) {
+    const Input = compose(
+        Cactus.observe('onChange'),
+        Cactus.withProps({ type: "text" }),
+    )('input');
+
+    function View({ name }: { name: string }) {
         return (
             <div>
-                <h1>Hello world!</h1>
-                <div>Welcome to hot-reloading React written in TypeScript!</div>
+                <div>Bon jour, { name }!</div>
+                <Input />
             </div>
-        );
+        )
     }
+
+    return Cactus.connectView(
+        View,
+        {
+            input: Cactus.from(Input),
+        },
+        model$
+    );
 }
+
+function main(sources: any) {
+    const actions = Cactus.selectable<any>(sources.events);
+    const inputChange$ = actions.select('input')
+        .map(({ value: ev }) => ({ name: ev.target.value }))
+        .startWith({ name: '' });
+
+    const { view$, events$ } = view(inputChange$);
+
+    return {
+        render: view$,
+        events: events$,
+    };
+}
+
+export default Cactus.appAsComponent(
+    main,
+    {
+        render: Cactus.makeReactComponentDriver(),
+        events: Cactus.makeEventDriver(),
+    },
+    'hello-world'
+);

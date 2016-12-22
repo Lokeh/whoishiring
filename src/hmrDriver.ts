0b2cd4b13128d8037b6ce9899cda625dc4c4c50b
Will <will@willacton.com>
2016-12-22 08:03:22 -0800

    comments in hmrDriver.ts

--- src/hmrDriver.ts
+++ src/hmrDriver.ts
@@ -10,28 +10,39 @@ interface Driver {
 
 function makeHotDriver(driver: Driver) {
     const hotSource = new Rx.ReplaySubject<any>();
-    let subscriber;
-    let tDispose;
-    return function hotProxy(sinks, key) {
-        if (subscriber) {
-            subscriber.unsubscribe();
-            tDispose();
+    let hotSubscriber;
+    let originalDispose;
+    return function hotProxyDriver(sinks, key) {
+        // if we're already running, dispose of our subscribers
+        if (hotSubscriber) {
+            hotSubscriber.unsubscribe();
+            originalDispose();
         }
+
+        // by remembering the outputs of the drivers, we hopefully
+        // avoid repeating side effects (which may not be idempotent)
         const { source, dispose } = driver(sinks, key);
-        tDispose = dispose;
-        subscriber = source.subscribe(hotSource);
-        const driverOutput = {
+        originalDispose = dispose;
+
+        // subscribe to our source with our ReplaySubject
+        // this will remember everything it emits, and then
+        // when a new subscriber adds itself (e.g. above!)
+        // it will replay all of it's events, in order
+        hotSubscriber = source.subscribe(hotSource);
+
+        // return our "hot" driver
+        return {
             source: hotSource,
             dispose: () => {
-                subscriber.unsubscribe();
+                hotSubscriber.unsubscribe();
                 dispose();
             }
         };
-        return driverOutput;
     }
 }
 
 export function makeHot(drivers) {
+    // apply makeHotDriver to each driver definition
     const hotDrivers = R.mapObjIndexed(makeHotDriver);
     return hotDrivers(drivers);
 }

30ab86e9a151f659efdccb507efa71de45e2cd3f
Will <will@willacton.com>
2016-12-21 22:48:48 -0800

    remove logging

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ export function main(sources: any) {
     const inputChange$ = actions.select('input')
         .map(({ value: ev }) => ({ name: ev.target.value }))
         .startWith({ name: '' })
-        .do((v) => console.log(v));
 
     const { view$, events$ } = view(inputChange$);
 
